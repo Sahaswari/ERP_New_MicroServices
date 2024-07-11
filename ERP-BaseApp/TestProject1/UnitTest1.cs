267bdc17d537aee21dc6d140eeb064bf1c2abb63
Madhushan11 <sakilamadhushanabc@gmail.com>
2024-07-12 00:09:26 +0530

    Upload Entity Changing Migration

---
+++ ERP-BaseApp/TestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Add your test logic here
+        }
+
+        // Adding 9999 more test methods
+        // This is a large file, so only a portion is shown for illustration
+        [Fact]
+        public void RandomTest1()
+        {
+            // Add your test logic here
+        }
+
+        [Fact]
+        public void RandomTest2()
+        {
+            // Add your test logic here
+        }
+
+        // ...
+
+        [Fact]
+        public void RandomTest9999()
+        {
+            // Add your test logic here
+        }
+    }
+}
--- ERP-BaseApp/src/StudentRegistration/ERP.StudentRegistration.DataService/obj/Debug/net8.0/ERP.StudentRegistration.DataService.AssemblyInfo.cs
+++ ERP-BaseApp/src/StudentRegistration/ERP.StudentRegistration.DataService/obj/Debug/net8.0/ERP.StudentRegistration.DataService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP.StudentRegistration.DataService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a58dfa065b3a3abf9b9efc805a582da1bb2096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe695685a82e075d78e9df63faddb4ac0df04f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP.StudentRegistration.DataService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP.StudentRegistration.DataService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
